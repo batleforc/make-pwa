d5723bf68ce5910449c0dcfe839aaf47009cf01a
Maxime <maxleriche.60@gmail.com>
2020-07-25 22:19:59 +0200

    ajout des free regular sur font awesome

--- src/Rapp/RApp.js
+++ src/Rapp/RApp.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far} from '@fortawesome/free-regular-svg-icons';
 
-library.add(fab, fas)
+library.add(fab, fas,far)
 
 ReactDOM.render(
   <React.StrictMode>
